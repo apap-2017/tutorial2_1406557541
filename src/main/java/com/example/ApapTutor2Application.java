6c152d7e31b07d8288b24d2defe954316c47d242
Komang Adelia Nala Ratih <komangadelianr@gmail.com>
2017-09-16 19:53:29 +0700

    Add files via upload

---
+++ src/main/java/com/example/ApapTutor2Application.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApapTutor2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApapTutor2Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/PageController.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+	@RequestMapping("/hello")
+	public String index() {
+		return "hello";
+	}
+
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "dunia") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+	@RequestMapping(value = { "/greeting", "greeting/{name}" })
+	public String greetingPath(@PathVariable Optional<String> name, Model model) {
+		if (name.isPresent()) {
+			model.addAttribute("name", name.get());
+		} else {
+			model.addAttribute("name", "dengklek");
+		}
+		return "greeting";
+	}
+	
+	@RequestMapping("/perkalian") 
+	public String kali(@RequestParam(value = "a", required = false, defaultValue = "0") Integer a, Model model1,
+	@RequestParam(value = "b", required = false, defaultValue = "0") Integer b, Model model2) {
+		model1.addAttribute("a", a);
+		model2.addAttribute("b", b);
+		return "perkalian";
+	}
+
+}
